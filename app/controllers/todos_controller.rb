ebb5b3f7069f0f4e408d7efc4d5fcf2d80c74bd1
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-09 11:10:05 -0300

    Added email column to todos and refactor email storage logic

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,7 @@
 class TodosController < ApplicationController
   before_action :authenticate
   def index
-    @todos = Todo.all
+    @todos = Todo.where(email: current_email)
   end
 
   def new
@@ -9,7 +9,8 @@ class TodosController < ApplicationController
   end
 
   def create
-    @todo = Todo.new(todo_params)
+    @todo = Todo.new(todo_params.merge(email: current_email))
+    # @todo.email = session[:current_email]
     if @todo.save
       redirect_to root_path
     else
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_155841) do
+ActiveRecord::Schema.define(version: 2021_04_09_125231) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
