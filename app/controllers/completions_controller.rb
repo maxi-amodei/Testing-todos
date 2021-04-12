b23ffabe6c1a4929a554171f0aa757d3ef8b0912
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-12 11:33:28 -0300

    refactoring todo model and test

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,6 +1,6 @@
 class CompletionsController < ApplicationController
   def create
-    todo.touch :completed_at
+    todo.complete!
     # Touch looks at a timestamp, sets current time and persists the record
     redirect_to root_path
   end
